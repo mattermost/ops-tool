13faec98cbfac063262ad5377cdaebf4b1d45c0b
Mustafa Kara <mustafa.kara@mattermost.com>
2022-06-24 18:41:57 +0300

    Limit joke types
    Signed-off-by: Mustafa Kara <mustafa.kara@mattermost.com>

--- scripts/fun/joke.sh
+++ scripts/fun/joke.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 timeout --preserve-status 1 \
-curl -s 'https://v2.jokeapi.dev/joke/Coding?type=single&format=json' \
+curl -s 'https://v2.jokeapi.dev/joke/Coding?type=single&format=json&blacklistFlags=nsfw,racist,sexist,explicit' \
 | jq --slurp -c '{data: .}'
